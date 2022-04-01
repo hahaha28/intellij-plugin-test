4c483af4df96e92a9e368766c1ece041286a4cad
GitHub Action <action@github.com>
2022-04-01 11:59:29 +0000

    Template cleanup

--- src/main/kotlin/com/github/hahaha28/intellijplugintest/MyBundle.kt
+++ src/main/kotlin/com/github/hahaha28/intellijplugintest/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.hahaha28.intellijplugintest
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/hahaha28/intellijplugintest/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.hahaha28.intellijplugintest.services
+
+import com.github.hahaha28.intellijplugintest.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
